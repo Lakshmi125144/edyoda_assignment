a343e7a1bd405c645bbe92f7cbbf54baf9e06af4
Lakshmi125144 <81343773+Lakshmi125144@users.noreply.github.com>
2023-09-12 23:46:37 +0530

    Add files via upload

---
+++ lambda_fn.py
@@ -0,0 +1,7 @@
+add_25 = lambda x: x + 25 
+# Sample input 
+sample_input = 10 
+# Call the lambda function with the sample input 
+result = add_25(sample_input) 
+# Print the result 
+print(result)
