c7da0d3f474b346b216a869b90dd6e78dd04088b
Lakshmi125144 <81343773+Lakshmi125144@users.noreply.github.com>
2023-10-02 22:29:55 +0530

    Update Linear_DS_3.py

---
+++ Linear_DS_3.py
@@ -0,0 +1,9 @@
+def are_rotations(string1, string2): 
+    """Check if two strings are a rotation of each other. Args: string1: A string. string2: A string. Returns: True if the strings are a rotation of each other, False otherwise. 
+    """ 
+    if len(string1) != len(string2): 
+        return False combined_string = string1 + string1 
+        return string2 in combined_string 
+    # Example usage: 
+    string1 = "waterbottle" string2 = "tlewaterbo" are_rotations = are_rotations(string1, string2) 
+    print(are_rotations)
