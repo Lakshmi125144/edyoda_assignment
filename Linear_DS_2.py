5d62e63a3af6d1fdb123375e400f79c4eef9ce7c
Lakshmi125144 <81343773+Lakshmi125144@users.noreply.github.com>
2023-10-02 22:16:37 +0530

    Add files via upload

---
+++ Linear_DS_2.py
@@ -0,0 +1,11 @@
+def reverse_array(array): 
+    """Reverse an array in place. Args: array: A list of integers. Returns: None. The array is reversed in place. 
+    """ 
+    start = 0 
+    end = len(array) - 1 
+    while start < end: 
+        array[start], array[end] = array[end], array[start] start += 1 end -= 1 
+        # Example usage: 
+    array = [1, 2, 3, 4, 5] 
+    reverse_array(array) 
+    print(array)
